3ac2e3c30b219e416420f0866d2b1b8e5b3008f2
Diogo Teles Sant'Anna <diogoteles@google.com>
2024-03-04 20:12:53 +0000

    move test data to external files, add new tests
    Signed-off-by: Diogo Teles Sant'Anna <diogoteles@google.com>

--- probes/hasDangerousWorkflowScriptInjection/patch/impl_test.go
+++ probes/hasDangerousWorkflowScriptInjection/patch/impl_test.go
@@ -16,6 +16,7 @@
 package hasDangerousWorkflowScriptInjection
 
 import (
+	"os"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -25,247 +26,62 @@ func Test_Run(t *testing.T) {
 	t.Parallel()
 	tests := []struct {
 		name     string
-		input    string
-		expected string
+		input_filepath    string
+		expected_filepath string
 		// err      error
 	}{
 		// Extracted from real Angular fix: https://github.com/angular/angular/pull/51026/files
 		{
-			name: "var: Comment Body",
-			input: `
-			name: Run benchmark comparison
-
-			on:
-				issue_comment:
-					types: [created]
-			
-			jobs:
-			  banchmark-compare:
-			  	steps:
-					- name: Preparing benchmark for GitHub action
-						id: info
-						run: yarn benchmarks prepare-for-github-action "${{github.event.comment.body}}"
-			`,
-			expected: `
-			name: Run benchmark comparison
-
-			on:
-				issue_comment:
-					types: [created]
-			
-			jobs:
-			  banchmark-compare:
-			  	steps:
-					- name: Preparing benchmark for GitHub action
-						id: info
-						env:
-							COMMENT_BODY: ${{ github.event.comment.body }}
-					    run: yarn benchmarks prepare-for-github-action "$COMMENT_BODY"
-			`,
+			name: "Real Example 1",
+			input_filepath: "realExample1.yaml",
+			expected_filepath: "realExample1_fixed.yaml",
 		},
 		// Inspired on a real fix: https://github.com/googleapis/google-cloud-go/pull/9011/files
 		{
-			name: "var: Pull Request Head Ref. Also uses untrusted input inside IF",
-			input: `
-			name: apidiff
-			on:
-				pull_request:
-			
-			permissions:
-				contents: read
-				pull-requests: write
-			
-			jobs:
-				apidiff:
-					needs: scan_changes
-					runs-on: ubuntu-latest
-					if: ${{ !needs.scan_changes.outputs.skip && !contains(github.event.pull_request.labels.*.name, 'breaking change allowed') }}
-					continue-on-error: true
-					strategy:
-						matrix: ${{ fromJson(needs.scan_changes.outputs.changed_dirs) }}
-					steps:
-						- name: Compare regenerated code to baseline
-						  run: |
-							cd ${{ matrix.changed }} && apidiff -m -incompatible ${{ steps.baseline.outputs.pkg }} . > diff.txt
-							if [[ ${{ github.event.pull_request.head.ref }} == owl-bot-copy ]]; then
-								sed -i '/: added/d' ./diff.txt
-							fi
-							cat diff.txt && ! [ -s diff.txt ]
-			`,
-			expected: `
-			name: apidiff
-			on:
-				pull_request:
-			
-			permissions:
-				contents: read
-				pull-requests: write
-			
-			jobs:
-				apidiff:
-					needs: scan_changes
-					runs-on: ubuntu-latest
-					if: ${{ !needs.scan_changes.outputs.skip && !contains(github.event.pull_request.labels.*.name, 'breaking change allowed') }}
-					continue-on-error: true
-					strategy:
-						matrix: ${{ fromJson(needs.scan_changes.outputs.changed_dirs) }}
-					steps:
-						- name: Compare regenerated code to baseline
-						  env:
-							PULL_REQUEST_HEAD_REF: ${{ github.event.pull_request.head.ref }}
-						  run: |
-							cd ${{ matrix.changed }} && apidiff -m -incompatible ${{ steps.baseline.outputs.pkg }} . > diff.txt
-							if [[ "$PR_HEAD_REF" == owl-bot-copy ]]; then
-								sed -i '/: added/d' ./diff.txt
-							fi
-							cat diff.txt && ! [ -s diff.txt ]
-			`,
+			name: "Real Example 2",
+			input_filepath: "realExample2.yaml",
+			expected_filepath: "realExample2_fixed.yaml",
 		},
 		// Inspired from a real lit/lit fix: https://github.com/lit/lit/pull/3669/files
 		{
-			name: "var: Pull Request Body",
-			input: `
-			name: Generate Release Image
-
-			on:
-				pull_request:
-					paths:
-					- '**/CHANGELOG.md'
-			
-			jobs:
-				release-image:
-					steps:
-						- name: Create release image
-						  run: |
-						    echo "${{ github.event.pull_request.body }}" > release.md
-			`,
-			expected: `
-			name: Generate Release Image
-
-			on:
-				pull_request:
-					paths:
-					- '**/CHANGELOG.md'
-			
-			jobs:
-				release-image:
-					steps:
-						- name: Create release image
-						  env:
-						  	PULL_REQUEST_BODY: ${{ github.event.pull_request.body }}
-						  run: |
-						    echo "$PULL_REQUEST_BODY" > release.md
-			`,
+			name: "Real Example 3",
+			input_filepath: "realExample3.yaml",
+			expected_filepath: "realExample3_fixed.yaml",
 		},
 		{
-			name: "Two incidences in different jobs. Vars: issue title and issue body",
-			input: `
-			on:
-				issue:
-			
-			jobs:
-				fascinating-job:
-					steps:
-						- name: it runs like magic
-						  run: |
-						    echo "${{ github.event.issue.title }}"
-
-				incredible-other-job:
-					steps:
-						- name: absolutely outstanding, safe as nothing else
-						  run: |
-						    echo "${{ github.event.issue.body }}" 
-			`,
-			expected: `
-			on:
-				issue:
-			
-			jobs:
-				fascinating-job:
-					steps:
-						- name: it runs like magic
-						  env:
-							ISSUE_TITLE: "${{ github.event.issue.title }}"
-						  run: |
-						    echo "$ISSUE_TITLE"
-
-				incredible-other-job:
-					steps:
-						- name: absolutely outstanding, safe as nothing else
-						  env:
-						    ISSUE_BODY: ${{ github.event.issue.body }}
-						  run: |
-							echo "$ISSUE_BODY" 
-			`,
+			name: "Test all (or most) types of user input that should be detected",
+			input_filepath: "allKindsOfUserInput.yaml",
+			expected_filepath: "allKindsOfUserInput_fixed.yaml",
 		},
 		{
-			name: "Two incidences in same job. Vars: discussion title and discussion body",
-			input: `
-			on:
-				discussion:
-					types: [created]
-			
-			jobs:
-				really-complete-job:
-					steps:
-						- name: it's only the beginning
-						  run: |
-						    echo "${{ github.event.discussion.title }}"
-						- name: ok, now we're talking
-						  run: |
-						    echo "${{ github.event.discussion.body }}"
-			`,
-			expected: `
-			on:
-				discussion:
-					types: [created]
-			
-			jobs:
-				really-complete-job:
-					steps:
-						- name: it's only the beginning
-						  env:
-						    DISCUSSION_TITLE: ${{ github.event.discussion.title }}
-						  run: |
-						    echo "$DISCUSSION_TITLE"
-						- name: ok, now we're talking
-						  env:
-						    DISCUSSION_BODY: ${{ github.event.discussion.body }}
-						  run: |
-						    echo "$DISCUSSION_BODY"
-			`,
+			name: "User's input is assigned to a variable before used",
+			input_filepath: "userInputAssignedToVariable.yaml",
+			expected_filepath: "userInputAssignedToVariable_fixed.yaml",
 		},
 		{
-			name: "Two incidences in same step. Vars: issue comment and fork name",
-			input: `
-			on:
-				fork
-				issue_comment:
-					types: [created, edited]
-			jobs:
-				solution-to-all-repo-problems:
-					steps:
-						- name: where things are done
-						  run: |
-						    echo "${{ github.event.issue_comment.comment }}"
-							mkdir "${{ github.event.fork.forkee.name }}"
-			`,
-			expected: `
-			on:
-				fork
-				issue_comment:
-					types: [created, edited]
-			jobs:
-				solution-to-all-repo-problems:
-					steps:
-						- name: where things are done
-						  env:
-						  	ISSUE_COMMENT_COMMENT: ${{ github.event.issue_comment.comment }}
-							FORK_FORKEE_NAME: ${{ github.event.fork.forkee.name }}
-						  run: |
-						    echo "$ISSUE_COMMENT_COMMENT"
-							mkdir "$FORK_FORKEE_NAME"
-			`,
+			name: "Two incidences in different jobs",
+			input_filepath: "twoInjectionsDifferentJobs.yaml",
+			expected_filepath: "twoInjectionsDifferentJobs_fixed.yaml",
+		},
+		{
+			name: "Two incidences in same job",
+			input_filepath: "twoInjectionsSameJob.yaml",
+			expected_filepath: "twoInjectionsSameJob_fixed.yaml",
+		},
+		{
+			name: "Two incidences in same step",
+			input_filepath: "twoInjectionsSameStep.yaml",
+			expected_filepath: "twoInjectionsSameStep_fixed.yaml",
+		},
+		{
+			name: "Bad indentation is kept the same",
+			input_filepath: "badIndentationMultipleInjections.yaml",
+			expected_filepath: "badIndentationMultipleInjections_fixed.yaml",
+		},
+		{
+			name: "A safe workflow is kept the same",
+			input_filepath: "safeExample.yaml",
+			expected_filepath: "safeExample.yaml",
 		},
 	}
 	for _, tt := range tests {
@@ -273,8 +89,17 @@ func Test_Run(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
-			output := Run(tt.input)
-			if diff := cmp.Diff(tt.expected, output); diff != "" {
+			input_content, err := os.ReadFile("./testdata/" + tt.input_filepath)
+			if err != nil {
+				t.Errorf("Couldn't read input testfile. Error:\n%s", err)
+			}
+
+			expected_content, err := os.ReadFile("./testdata/" + tt.expected_filepath)
+			if err != nil {
+				t.Errorf("Couldn't read expected testfile. Error:\n%s", err)
+			}
+			output := Run(string(input_content[:]))
+			if diff := cmp.Diff(string(expected_content[:]), output); diff != "" {
 				t.Errorf("mismatch (-want +got):\n%s", diff)
 			}
 		})
